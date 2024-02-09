1ce381edb2008b2fa7b436e0623eefcf4d4e5ba3
Alex Militaru <militaru.a.alexandru@gmail.com>
2024-02-09 12:58:47 +0200

    Filters and others functionalities implemented - almost done

--- ProiectDAW_IP/Controllers/ProdusController.cs
+++ ProiectDAW_IP/Controllers/ProdusController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProiectDAW_IP.ContextModels;
+using ProiectDAW_IP.Enums;
 using ProiectDAW_IP.Models;
 using System.Drawing;
 
@@ -86,11 +87,86 @@ namespace ProiectDAW_IP.Controllers
         }
 
 
-        [HttpGet]
-        public IActionResult ViewAllUsers()
+        //[HttpGet]
+        //public IActionResult ViewAllUsers()
+        //{
+        //    var produse = _context.Produse.
+        //    return View(produse);
+        //}
+        public IActionResult ViewAllUsers(string sortOrder)
         {
-            var produse = _context.Produse.Include(s => s.Categorie).ToList();
-            return View(produse);
+            // Sortare în funcție de valoarea parametrului sortOrder
+            IQueryable<Produs> productsQuery = _context.Produse;
+
+            switch (sortOrder)
+            {
+                case "PretDescrescator":
+                    productsQuery = productsQuery.Include(p => p.Categorie).OrderByDescending(p => p.Pret);
+                    break;
+                case "PretCrescator":
+                    productsQuery = productsQuery.Include(p => p.Categorie).OrderBy(p => p.Pret);
+                    break;
+
+                default: // Implicit, doar afisare
+                    productsQuery = productsQuery.Include(s => s.Categorie);
+                    break;
+            }
+
+            var products = productsQuery.ToList();
+            return View(products);
         }
     }
+
+
+
+
+
+
+    //public IActionResult ViewAllUsers(string sortOrder, string searchUserName)
+    //{
+    //    IQueryable<User> usersQuery = _context.Users.Where(u => u.UserType == UserType.Client);
+
+    //    // Search by username if the searchUserName parameter is not null or empty
+    //    if (!string.IsNullOrEmpty(searchUserName))
+    //    {
+    //        usersQuery = usersQuery.Where(u => u.UserName.Contains(searchUserName));
+    //    }
+
+    //    // Sort by score or username based on the sortOrder parameter
+    //    switch (sortOrder)
+    //    {
+    //        case "ProdusName":
+    //            usersQuery = usersQuery.OrderBy(p => p.);
+    //            break;
+    //        case "Score":
+    //        default:
+    //            usersQuery = usersQuery.OrderByDescending(u => u.Score);
+    //            break;
+    //    }
+
+    //    List<User> sortedUsers = usersQuery.ToList();
+    //    return View(sortedUsers);
+    //}
+
+
+    //public IActionResult PretDescrescator()
+    //    {
+    //        var ProduseDesc = _context.Produse.Include(c => c.Categorie).OrderByDescending(c => c.Pret).ToList();
+
+    //        return View(ProduseDesc);
+    //    }
+
+    //    public IActionResult PretCrescator()
+    //    {
+    //        var ProduseDesc = _context.Produse.Include(c => c.Categorie).OrderBy(c => c.Pret).ToList();
+
+    //        return View(ProduseDesc);
+    //    }
+
+
+
+
+
+
+    
 }
